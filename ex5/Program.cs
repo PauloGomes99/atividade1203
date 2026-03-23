4e74e6c9fb9e187a25044dd8387164e708189b0e
Paulo <paulo.1999sep@gmail.com>
2026-03-22 21:27:09 -0300

    ex5

---
+++ ex5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade3
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int n1;
+            int r;
+
+            
+
+
+
+            Console.WriteLine("Digite um numero");
+            n1 = int.Parse(Console.ReadLine());
+
+            r = n1 + n1 * 2;
+
+            Console.WriteLine("O nuemro é:"+r);
+
+        }
+    }
+}

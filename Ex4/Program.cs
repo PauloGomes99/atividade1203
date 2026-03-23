3827f0b7f9524c6b635f60e028ab93c910c374fe
Paulo <paulo.1999sep@gmail.com>
2026-03-22 21:11:57 -0300

    ex4

---
+++ Ex4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade3
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            int c;
+
+            int f;
+
+
+
+            Console.WriteLine("Cº:");
+            c = int.Parse(Console.ReadLine());
+
+            f = c . 9/5 + 32;
+
+            Console.WriteLine("Fº:" + c);
+            
+        }
+    }
+}

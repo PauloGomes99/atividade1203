4ebea8f80d8fa17c35650666f225dca6551c7823
Paulo <paulo.1999sep@gmail.com>
2026-03-22 21:59:45 -0300

    ex6

--- ex6/Program.cs
+++ ex6/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio6
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            double dolar;
+            double cotacao = 5.30;
+            double reais;
+
+
+            Console.WriteLine("US$");
+            dolar = double.Parse(Console.ReadLine());
+
+
+            reais = dolar * cotacao;
+
+            Console.WriteLine("R$:" +reais);
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}

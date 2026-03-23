2b8d50b562c3f37dba4a3d3c547cf48ea954ceb0
Paulo <paulo.1999sep@gmail.com>
2026-03-22 21:58:48 -0300

    ex4

--- Ex4/obj/Debug/net10.0/Ex4.AssemblyInfo.cs
+++ Ex4/obj/Debug/net10.0/Ex4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3827f0b7f9524c6b635f60e028ab93c910c374fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac3976c5177f241580403ff00a2272da8f61e86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
